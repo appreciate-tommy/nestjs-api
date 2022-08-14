032be9db7eb2f5ad662eaef5f482caf0966ad345
sjtommylee <sjtommylee@gmail.com>
2022-08-13 21:06:22 -0700

    auth wip

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,19 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 //controller needs to call the service.
 
 @Controller('auth')
 export class AuthController {
   // having the private here means that the service can only be used within this class.
+
   constructor(private authService: AuthService) {}
   @Post('signup')
-  signup() {
+  signup(@Body() dto: AuthDto) {
+    console.log({
+      dto: dto,
+    });
     return this.authService.signup();
   }
 
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const globalPipes = new ValidationPipe();
+  app.useGlobalPipes(globalPipes);
+
   await app.listen(3333);
 }
 bootstrap();
