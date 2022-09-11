4878ac8471389bfcfdcdb372b6bdbba6cb2e3439
sjtommylee <sjtommylee@gmail.com>
2022-09-11 03:08:07 -0700

    JWT, RoleGuards

---
+++ src/auth/validator/jwt.strategy.ts
@@ -0,0 +1,14 @@
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(config: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: config.get('JWT_SECRET'),
+    });
+  }
+}
